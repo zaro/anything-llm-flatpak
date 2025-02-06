1805a5e9ff95979957de403378b635f180527812
Svetlozar Argirov <zarrro@gmail.com>
2025-02-06 22:56:12 +0200

    fix udpate-version script being blocked

--- update-version.py
+++ update-version.py
@@ -1,15 +1,30 @@
 #!/user/bin/python
+import subprocess
 import urllib.request
+import re
+import os
 import email.utils
 import hashlib
 import json
 import xml.etree.ElementTree
 
+
 app_image_url = "https://cdn.anythingllm.com/latest/AnythingLLMDesktop.AppImage"
 check_version_url = "https://cdn.anythingllm.com/latest/version.txt"
-f = urllib.request.urlopen(check_version_url)
-last_modified_tuple = email.utils.parsedate(f.info()['Last-Modified'])
-latest_version = f.read().decode('ascii')
+result = subprocess.run(['curl', '-si', check_version_url], stdout=subprocess.PIPE)
+output_lines = result.stdout.decode().splitlines()
+for line in output_lines:
+    m = re.match('^last-modified: (.*)', line)
+    if m:
+        last_modified_str = m.group(1)
+        break
+
+if not last_modified_str:
+    print("Failed to get last-modified")
+    exit(1)
+
+last_modified_tuple = email.utils.parsedate(last_modified_str)
+latest_version = output_lines[-1].strip()
 last_modified = f"{last_modified_tuple[0]}-{last_modified_tuple[1]:02}-{last_modified_tuple[2]:02}"
 latest = (latest_version, last_modified)
 
@@ -32,15 +47,17 @@ print("Calcualte last release sha256...")
 # Open the URL and read its content
 app_image_sha256_hash = hashlib.sha256()
 
+file_name = 'AnythingLLMDesktop.AppImage'
 # Open the URL and read the content in chunks
-with urllib.request.urlopen(app_image_url) as response:
+result = subprocess.run(['curl', '-Lo', file_name, app_image_url], stdout=subprocess.PIPE)
+with open(file_name, 'rb') as response:
     while True:
         chunk = response.read(1024 * 1024)
         if not chunk:
             break
         # Update the hash with the current chunk
         app_image_sha256_hash.update(chunk)
-
+os.remove(file_name)
 # Return the final hash as a hexadecimal string
 latest_version_sha256 = app_image_sha256_hash.hexdigest()
 print("Done.")
